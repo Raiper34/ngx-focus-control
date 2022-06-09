d390dbc42306c70e22235a540627481ab603a2ea
Raiper34 <raipergm34@gmail.com>
2022-06-09 16:42:14 +0200

    remove(focus-parent): remove focus parent directive

--- projects/ngx-focus-control/src/public-api.ts
+++ projects/ngx-focus-control/src/public-api.ts
@@ -4,7 +4,6 @@
 export * from './lib/directives/focus-auto.directive';
 export * from './lib/directives/focus-control.directive';
 export * from './lib/directives/focus-selector.directive';
-export * from './lib/directives/focus-parent.directive';
 export * from './lib/directives/focus-group.directive';
 export * from './lib/directives/focus-lock.directive';
 export * from './lib/directives/focus-if.directive';
